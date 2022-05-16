6c1c6f7d550765064385251ee5dc1d93de824f0c
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-05-16 16:21:18 +0300

    okey

---
+++ DelegateFactories/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace DelegateFactories
+{
+    public class Loan
+    {
+        public string Currency { get; set; }
+        public int Amount { get; set; }
+
+        public Loan(string currency, int amount)
+        {
+            if (currency == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(currency));
+            }
+            Currency = currency;
+            Amount = amount;
+        }
+
+
+    }
+
+    public class DelegateFactories
+    {
+        // Func<T>
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
